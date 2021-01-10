f7fc1ad58aec113652d593fd38ff5c8b8e26a421
Peter <peter.lim91@gmail.com>
2021-01-10 14:27:19 +1300

    refactor createBoard

--- createBoard.js
+++ createBoard.js
@@ -1,16 +1,13 @@
 function createBoard(size) {
-
-  function createBoard(size) {
-    let board = []
-    for (let i = 0; i < size; i++) {
-      let row = createRow(size)
-      // for (let cell of row) {
-        // cell.row = i
-      // }
-      board.push(row)
-    }
-    return board
+  let board = []
+  for (let i = 0; i < size; i++) {
+    let row = createRow(size)
+    // for (let cell of row) {
+    // cell.row = i
+    // }
+    board.push(row)
   }
+  return board
 
   function createRow(size) {
     let row = []
@@ -19,29 +16,6 @@ function createBoard(size) {
     }
     return row
   }
-
-
-  // console.log(createBoard(size))
-  return createBoard(size)
-
-
-
-
-
-
-
-  //   function createRow (size) {
-  //   return Array(size).fill(
-  //     {
-  //       currentState: false,
-  //       nextState: false
-  //     }
-  //   ) 
-  //  }
-
-  //   let board = Array(size).fill(createRow(size))
-  //   console.log(board)
-  //   return board
 }
 
 module.exports = createBoard
