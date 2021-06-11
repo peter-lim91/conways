afad3a3bfebc414a95704fe840731e055da06dab
Peter <peter.lim91@gmail.com>
2021-06-11 17:52:45 +1200

    change ws url

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-const express = require('express');
-const WebSocket = require('ws');
-const { chunk } = require('lodash')
-const cw = require('./server/conways/conways.js')
+const express = require("express");
+const WebSocket = require("ws");
+const { chunk } = require("lodash");
+const cw = require("./server/conways/conways.js");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3020;
 const app = express();
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-const server = app.listen(PORT, () => console.log('listening on port ' + PORT));
+const server = app.listen(PORT, () => console.log("listening on port " + PORT));
 const wss = new WebSocket.Server({ noServer: true });
-server.on('upgrade', (req, socket, head) => {
+server.on("upgrade", (req, socket, head) => {
   wss.handleUpgrade(req, socket, head, (ws) => {
-    wss.emit('connection', ws, req)
-  })
-})
+    wss.emit("connection", ws, req);
+  });
+});
 
-wss.on('connection', (ws) => {
-  ws.on('message',(message) => {
-    const boardInfo = JSON.parse(message)
-    const currentBoard = chunk(boardInfo.board, boardInfo.width)
-    const nextBoard = (cw.nextBoard(currentBoard))
-    const nextBoardFlat = nextBoard.flat()
-    const payload = JSON.stringify(nextBoardFlat)
-    ws.send(payload)
-  })
-})
-
-
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    const boardInfo = JSON.parse(message);
+    const currentBoard = chunk(boardInfo.board, boardInfo.width);
+    const nextBoard = cw.nextBoard(currentBoard);
+    const nextBoardFlat = nextBoard.flat();
+    const payload = JSON.stringify(nextBoardFlat);
+    ws.send(payload);
+  });
+});
--- public/websocket.js
+++ public/websocket.js
@@ -1,8 +1,7 @@
-const HOST = location.origin.replace(/^http/, 'ws');
+const HOST = location.href.replace(/^http/, "ws");
 const socket = new WebSocket(HOST);
 
-socket.addEventListener('message', function (ev) {
-  const newBoardFlat = JSON.parse(ev.data)
-  displayBoard(newBoardFlat)
+socket.addEventListener("message", function (ev) {
+  const newBoardFlat = JSON.parse(ev.data);
+  displayBoard(newBoardFlat);
 });
-
